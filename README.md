e6c3a6cdf49a2d85595c324812b7392e5a85e25d
Michael <explodingdinosaurs@gmail.com>
2020-03-17 18:08:01 +1100

    Added plot of breakdown of australia by state/territory. Updated install requirements.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Plotting COVID-19 case data from the WHO.
 
 ## Setup
-pip install pandas numpy plotly==4.5.4 matplotlib
+pip install pandas numpy plotly==4.5.4 matplotlib wikipedia
 
---
+++ aus_states.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import plotly.graph_objects as go
+import wikipedia as wp
+import re
+
+# Get the data
+html = wp.page("2020_coronavirus_pandemic_in_Australia").html().encode("UTF-8")
+df = pd.read_html(html)[4]
+df = df.rename(columns={'Unnamed: 0': 'date'})
+df = df.set_index('date')
+
+# Clean the references from the data
+for state in list(df):
+    try:
+        df[state] = df[state].str.replace(r"\[.*\]","")
+    except AttributeError:
+        pass
+
+# Clean the references from the column names
+for state in list(df):
+    try:
+        new_state = re.sub("[\(\[].*?[\)\]]", "", state)
+        df = df.rename(columns={state: new_state})
+    except AttributeError:
+        pass
+
+# Let's plot this mofo    
+fig = go.Figure()
+
+# Plot all the states!
+for state in list(df):
+    fig.add_trace(go.Scatter(
+        x=df.index,
+        y=df[state],
+        name=state,
+    ))
+
+# Make the plot look fancy. 
+fig.update_layout(title='COVID-19 Cases by State/Territory in Austalia',
+                   xaxis_title='Date',
+                   yaxis_title='Cases')
+
+    
+fig.show()
+
