70ab2efd7334c502fc7648c11265657ab5dfbf4c
explodingdinosaurs <explodingdinosaurs@gmail.com>
2020-03-18 16:22:31 +1100

    Updated Sara's log plotting file. Changed to log10 instead of log2. Removed extra plot command at end of file, as it was creating a temp file. Added line to turn zeros into NaNs so that we don't get divide by 0 errors.

---
+++ log_plot_over_300_cases_today.py
@@ -0,0 +1,52 @@
+"""Plot only log10 for Countries that have over 300 cases today.
+
+Linear behaviour indicated exponential growth
+"""
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.offline import plot #I added offline plot  
+
+# Threshold number of cases to plot that country
+Tresh_Cases = 300
+
+# Get the data
+data_address = 'https://covid.ourworldindata.org/data/total_cases.csv'
+df = pd.read_csv(data_address)
+df = df.set_index('date')
+
+#Select only Countries that have more than Tresh_Cases cases today
+more_than_100_today = (df[:][-1:] > Tresh_Cases)
+df_relevant = df[df.columns[more_than_100_today.iloc[0,:]]]
+
+#----  Periods with less than 30 cases are ignored ----
+df_30_bool = df_relevant > 30
+# Set entries < 30 to 0
+df_relevant = df_relevant * df_30_bool
+# Set entry of 0 to NaN - stops divide by 0 errors
+df_relevant = df_relevant.replace({0:np.nan})
+
+# Remove date index
+df_relevant = df_relevant.reset_index(drop=True)
+
+# Drop the International data.. is it cruise ships???
+df_relevant = df_relevant.drop(['International'], axis=1)
+
+# Plot the data
+fig = go.Figure()
+
+for country in list(df_relevant):
+    fig.add_trace(go.Scatter(
+        x=df_relevant.index,
+        y=np.log10(df_relevant[country]),
+        name=country,
+    ))
+
+# Make the plot look fancy. 
+fig.update_layout(title='SARS-CoV-2 Cases in Logarithmic scale: Linear behaviour indicates exponential growth',
+                   xaxis_title='Day Number',
+                   yaxis_title='Log10(Cases)')
+    
+fig.show()
+
